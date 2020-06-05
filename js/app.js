9d91bc1e1e17441607979e7a6aa3822add97f13e
Omario169 <47423327+Omario169@users.noreply.github.com>
2020-06-05 03:47:44 +0100

    new API key

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
 
 $(document).ready(function () {
 
-    var key = 'AIzaSyB6b6JzXJrAxGaPL19Y8z47Kq28nyt2ESU';
+    var key = 'AIzaSyDjbIASMKMo9yYmS-wjtyvL8shVadN3OL8';
     var playlistId = 'PLGI2vA9a3Tt7ZiLJnOs2LwCrdtkxvhO1J';
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
@@ -78,7 +78,7 @@ $(document).ready(function () {
 
     
 
-    // https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyB6b6JzXJrAxGaPL19Y8z47Kq28nyt2ESU&&textFormat=plainText&part=snippet&allThreadsRelatedToChannelId=UCCpldBpw1eBel90tlxI04Sg
+    // https://www.googleapis.com/youtube/v3/commentThreads?key=AIzaSyDjbIASMKMo9yYmS-wjtyvL8shVadN3OL8&&textFormat=plainText&part=snippet&allThreadsRelatedToChannelId=UCCpldBpw1eBel90tlxI04Sg
 
 });
 
@@ -90,7 +90,7 @@ $(document).ready(function () {
 
     var URLcomments = 'https://www.googleapis.com/youtube/v3/commentThreads';
     var allThreadsRelatedToChannelId= 'UCCpldBpw1eBel90tlxI04Sg';
-    var key = 'AIzaSyB6b6JzXJrAxGaPL19Y8z47Kq28nyt2ESU';
+    var key = 'AIzaSyDjbIASMKMo9yYmS-wjtyvL8shVadN3OL8';
 
 
     var optionsComment = {
@@ -105,9 +105,50 @@ $(document).ready(function () {
 
     function loadComments() {
         $.getJSON(URLcomments, optionsComment, function(dataComments) {
-            console.log(dataComments)
+            var idComments = dataComments;
+            console.log(idComments);
         })
     }
 
 
 });
+
+
+// function loadVids() {
+//     $.getJSON(URL, options, function (data) {
+//         var id = data.items[0].snippet.resourceId.videoId;
+//         mainVid(id);
+//         resultsLoop(data);
+//     });
+// }
+
+// function mainVid(id) {
+//     $('#video').html(`
+//                 <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+//             `);
+// }
+
+    
+// function resultsLoop(data) {
+
+//     $.each(data.items, function (i, item) {
+
+//         var thumb = item.snippet.thumbnails.medium.url;
+//         var title = item.snippet.title;
+//         var desc = item.snippet.description.substring(0, 100);
+//         var vid = item.snippet.resourceId.videoId;
+
+
+//         $('main').append(`
+//                         <article class="item" data-key="${vid}">
+
+//                             <img src="${thumb}" alt="" class="thumb">
+//                             <div class="details">
+//                                 <h4>${title}</h4>
+//                                 <p>${desc}</p>
+//                             </div>
+
+//                         </article>
+//                     `);
+//     });
+// }
